cedee0b63948ee7ff9b1f220b6f77f969eb5c439
John <j.x.chapman@gmail.com>
2015-10-28 16:55:01 +0100

    plang::HelloWorld() prints compiler def

--- src/plang/plang.cpp
+++ src/plang/plang.cpp
@@ -10,5 +10,11 @@ void plang::HelloWorld()
 #ifdef PLANG_DEBUG
 	std::cout << "PLANG_DEBUG" << std::endl;
 #endif
-	std::cout << "plang says hello, world!" << std::endl;
+#ifdef PLANG_COMPILER_GNU
+	std::cout << "PLANG_COMPILER_GNU" << std::endl;
+#endif
+#ifdef PLANG_COMPILER_MSVC
+	std::cout << "PLANG_COMPILER_MSVC" << std::endl;
+#endif
+	std::cout << "Hello, world!" << std::endl;
 }
