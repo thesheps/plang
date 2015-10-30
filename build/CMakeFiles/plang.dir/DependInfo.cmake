6b3dbbdffc8127d35375f2b6d7b12d6dc7951a85
shepc <chris.shepherd@ao.com>
2015-10-30 08:07:52 +0000

    Fixing CMake to use gtest

---
+++ build/CMakeFiles/plang.dir/DependInfo.cmake
@@ -0,0 +1,18 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/source/plang/src/plang/plang.cpp" "C:/source/plang/build/CMakeFiles/plang.dir/src/plang/plang.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/CMakeFiles/plang_unit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/plang_unit.dir/src/plang_unit/main.cpp.obj"
+  "plang_unit.pdb"
+  "plang_unit.exe"
+  "plang_unit.exe.manifest"
+  "libplang_unit.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/plang_unit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
