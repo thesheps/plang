fa7d8d4e3bdd4dab99fca21198416d9414a827db
Chris Shepherd <thesheps@gmail.com>
2015-11-02 23:54:23 +0000

    Fixing merge hell.  @john-chapman - hopefully this should be the definitive folder structure now!  Had a derp moment and ended up pushing the merge files.  Should all be sorted now, but if you could do a quick code review that would be great.  Similarly if I've accidentally binned off some of your suggestions, please reinstate!

--- src/plang_go/main.cpp
+++ src/plang_go/main.cpp
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-=======
-#include <plang/plang.h>
-
->>>>>>> 8dc874e9e7889c901f29e0eebd679e0aeb521ce2
 #include <iostream>
 
+#include <plang/plang.h>
 #include <plang/parser.h>
 
 using namespace plang;
@@ -13,24 +9,16 @@ int main(int argc, char** argv)
 {
   Parser parser = Parser();
 
-<<<<<<< HEAD
-  while(1 == 1)
-  {
-    std::string input;
-    std::cout << "> ";
-    getline (std::cin, input);
-=======
 	while (true) {
-		std::string input;
+  		std::string input;
   		std::cout << "> ";
   		getline(std::cin, input);
->>>>>>> 8dc874e9e7889c901f29e0eebd679e0aeb521ce2
 
-    if (input == "exit")
-    {
-      return 0;
-    }
+      if (input == "exit")
+      {
+        return 0;
+      }
 
-    std::cout << parser.parse(input) << "\n";
+      std::cout << parser.parse(input) << "\n";
   }
 }
