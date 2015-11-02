1502fa9de580d0441f8f8d9d284735689d5b0a56
Chris Shepherd <thesheps@gmail.com>
2015-11-02 20:47:00 +0000

    Broke the build again, updated build.bat with error handling

--- build.bat
+++ build.bat
@@ -1,5 +1,14 @@
+@echo off
+
 mkdir build
 cd build
+
 cmake .. -G "MinGW Makefiles"
+if %ERRORLEVEL% GEQ 1 GOTO Error
+
 mingw32-make.exe
+if %ERRORLEVEL% GEQ 1 GOTO Error
+
 ctest --output-on-failure
+
+:Error
