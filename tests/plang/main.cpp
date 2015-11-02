95312df031aec1e7c7d145d10d2d98c96791c5aa
Chris Shepherd <thesheps@gmail.com>
2015-11-02 20:47:00 +0000

    Broke the build again, updated build.bat with error handling

--- tests/plang/main.cpp
+++ tests/plang/main.cpp
@@ -18,3 +18,9 @@ TEST_CASE("When I parse a mathematical addition, then the results are correct.",
 	string output = parser.parse("1 + 2");
 	REQUIRE(output == "3");
 }
+
+TEST_CASE("When I parse a different mathematical addition, then the results are correct.", "[parsing][maths]") {
+	Parser parser = Parser();
+	string output = parser.parse("2 + 2");
+	REQUIRE(output == "4");
+}
