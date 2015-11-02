5b27e74b2e68c9dc8d98356814e56884b5bcffb9
shepc <chris.shepherd@ao.com>
2015-11-02 09:21:37 +0000

    REPL spike

--- src/plang/plang.h
+++ src/plang/plang.h
@@ -9,25 +9,24 @@
 
 #include <cstddef> // size_t
 #include <cstdint> // sized ints
+#include <string>
+
+using std::string;
 
 namespace plang {
+	typedef int8_t          sint8;
+	typedef uint8_t         uint8;
+	typedef int16_t         sint16;
+	typedef uint16_t        uint16;
+	typedef int32_t         sint32;
+	typedef uint32_t        uint32;
+	typedef int64_t         sint64;
+	typedef uint64_t        uint64;
 
-typedef int8_t          sint8;
-typedef uint8_t         uint8;
-typedef int16_t         sint16;
-typedef uint16_t        uint16;
-typedef int32_t         sint32;
-typedef uint32_t        uint32;
-typedef int64_t         sint64;
-typedef uint64_t        uint64;
+	typedef std::ptrdiff_t  sint;
+	typedef std::size_t     uint;
 
-typedef std::ptrdiff_t  sint;
-typedef std::size_t     uint;
-
-////////////////////////////////////////////////////////////////////////////////
-/// \todo Remove!
-void HelloWorld();
-
-} // namespace plang
+	std::string Parse(string input);
+}
 
 #endif // plang_h
