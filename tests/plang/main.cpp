e9573254f48075e44348cf34ef697ca1ed2182a4
John Chapman <contact@john-chapman.net>
2015-11-07 19:06:58 +0100

    Fixed trailing whitespace bug in TokenStream; added more TokenStream tests

--- tests/plang/main.cpp
+++ tests/plang/main.cpp
@@ -18,3 +18,29 @@ TEST_CASE("When I parse plaintext language, then results are tokenised as separa
 	t = tokstr.getNext();
 	REQUIRE(t == Token("world"));
 }
+
+TEST_CASE("Parsing correctly eliminates leading/trailing whitespace", "[parsing]") {
+	char buf[] = "  tok0 tok1   ";
+	TokenStream tokstr(buf);
+	Token t;
+
+	t = tokstr.getNext();
+	REQUIRE(t == Token("tok0"));
+	t = tokstr.getNext();
+	REQUIRE(t == Token("tok1"));
+	t = tokstr.getNext();
+	REQUIRE(t == Token::Eos);
+}
+
+TEST_CASE("Parsing correctly handles consecutive optokens", "[parsing]") {
+	char buf[] = "+-";
+	TokenStream tokstr(buf);
+	Token t;
+
+	t = tokstr.getNext();
+	REQUIRE(t == Token("+"));
+	t = tokstr.getNext();
+	REQUIRE(t == Token("-"));
+	t = tokstr.getNext();
+	REQUIRE(t == Token::Eos);
+}
