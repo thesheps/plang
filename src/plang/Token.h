395ca96c9b532a57157cfc828fd77d2f7ad4c89b
John Chapman <contact@john-chapman.net>
2015-11-07 19:17:32 +0100

    Added asserts. This fixes #11

--- src/plang/Token.h
+++ src/plang/Token.h
@@ -1,13 +1,15 @@
 #ifndef plang_token_h
 #define plang_token_h
 
+#include <plang/def.h>
+
 namespace plang {
 	class Token {
 		public:
 			static Token Eos;
 
 			Token(): m_tok(0) {}
-			Token(const char* _tok): m_tok(_tok) {};
+			Token(const char* _tok): m_tok(_tok) {  PLANG_ASSERT(m_tok); };
 
 			bool operator==(const Token& _rhs) const;
 			bool operator!=(const Token& _rhs) const { return !(*this == _rhs); }
--- src/plang/def.h
+++ src/plang/def.h
@@ -10,6 +10,29 @@
 #include <cstddef> // size_t
 #include <cstdint> // sized ints
 
+
+#define PLANG_UNUSED(x)        do { (void)sizeof(x); } while(0)
+#define PLANG_STATIC_ASSERT(e) { (void)sizeof(char[(e) ? 1 : -1]); }
+#ifdef PLANG_DEBUG
+	#ifdef PLANG_COMPILER_MSVC
+		#define PLANG_BREAK() __debugbreak()
+	#else
+		#include <cstdlib>
+		#define PLANG_BREAK() abort()
+	#endif
+
+	#define PLANG_ASSERT(e) \
+		do { \
+			if (!(e)) { \
+				PLANG_BREAK(); \
+			} \
+		} while(0)
+	#define PLANG_VERIFY(e) PLANG_ASSERT(e)
+#else
+	#define PLANG_ASSERT(e) do { PLANG_UNUSED(e); } while(0)
+	#define PLANG_VERIFY(e) (void)(e)
+#endif
+
 namespace plang {
 	typedef int8_t          sint8;
 	typedef uint8_t         uint8;
